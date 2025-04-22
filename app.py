935709cc4d270d45eb20769f1ccb45a89953fc1e
SageGJ <gianna.jordan@sagebase.org>
2025-04-22 13:55:02 -0700

    update schematic version

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ match environment:
             "FQDN": "stage.schematic.io",
             "CERTIFICATE_ARN": "arn:aws:acm:us-east-1:878654265857:certificate/d11fba3c-1957-48ba-9be0-8b1f460ee970",
             "TAGS": {"CostCenter": "NO PROGRAM / 000000"},
-            "SCHEMATIC_CONTAINER_LOCATION": "ghcr.io/sage-bionetworks/schematic:v25.2.2",
+            "SCHEMATIC_CONTAINER_LOCATION": "ghcr.io/sage-bionetworks/schematic:v25.4.1",
         }
     case "dev":
         environment_variables = {
@@ -34,7 +34,7 @@ match environment:
             "FQDN": "dev.schematic.io",
             "CERTIFICATE_ARN": "arn:aws:acm:us-east-1:631692904429:certificate/0e9682f6-3ffa-46fb-9671-b6349f5164d6",
             "TAGS": {"CostCenter": "NO PROGRAM / 000000"},
-            "SCHEMATIC_CONTAINER_LOCATION": "ghcr.io/sage-bionetworks/schematic:v25.2.2",
+            "SCHEMATIC_CONTAINER_LOCATION": "ghcr.io/sage-bionetworks/schematic:v25.4.1",
         }
     case _:
         valid_envs_str = ",".join(VALID_ENVIRONMENTS)
